572a25fcf6a064539d571e365b4ed1bd73912242
root <root@raspberrypi>
2018-05-31 13:54:59 +0200

    testing serial bluetooth transfer

--- bluepy-serial/test.py
+++ bluepy-serial/test.py
@@ -2,13 +2,14 @@ from bluepy.btle import *
 
 
 # connect to device
-per = Peripheral('F5:87:4A:AD:6B:DD', 'random')
+per = Peripheral('DA:E2:C9:AB:2F:65', "random")
+#per = Peripheral('F5:87:4A:AD:6B:DD', "random")
 
 try:
 
     
     # read test string on uart 00003
-    for x in range(0,10):
+    for x in range(0,1000):
         c = per.getCharacteristics(uuid='6e400003b5a3f393e0a9e50e24dcca9e')[0]
         print c.read()
 
